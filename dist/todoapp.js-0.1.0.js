a099003cba3e86c17c207fa55f0244c02625d684
Erik Trom <erik.trom@gmail.com>
2013-06-05 20:20:18 -1000

    remove test templates

--- dist/todoapp.js-0.1.0.js
+++ dist/todoapp.js-0.1.0.js
@@ -47,16 +47,6 @@ define("todoapp", ["exports"], function(__exports__) {
   return __exports__.Todoapp = Todoapp;
 });
 
-Ember.TEMPLATES["grandparent/parent/child"] = Ember.Handlebars.template(function anonymous(Handlebars,depth0,helpers,partials,data) {
-this.compilerInfo = [3,'>= 1.0.0-rc.4'];
-helpers = helpers || Ember.Handlebars.helpers; data = data || {};
-  
-
-
-  data.buffer.push("Should be nested.");
-  
-});
-
 Ember.TEMPLATES["simple"] = Ember.Handlebars.template(function anonymous(Handlebars,depth0,helpers,partials,data) {
 this.compilerInfo = [3,'>= 1.0.0-rc.4'];
 helpers = helpers || Ember.Handlebars.helpers; data = data || {};
