2230ed98acc9afaddea3959db8f559bc483286ea
Erik Trom <erik.trom@gmail.com>
2013-06-05 20:54:26 -1000

    cleanup, remove dist from git, remove fake simple template

--- app/todoapp.coffee
+++ app/todoapp.coffee
@@ -1,3 +1,6 @@
 Todoapp = Em.Application.create();
+Todoapp.Store = DS.Store.extend
+  revision: 13
+  adapter: 'DS.FixtureAdapter'
 
 export Todoapp
--- dist/todoapp.js-0.1.0.amd.js
+++
@@ -1,6 +0,0 @@
-define("todoapp", ["exports"], function(__exports__) {
-  "use strict";
-  var Todoapp;
-  Todoapp = Em.Application.create();
-  return __exports__.Todoapp = Todoapp;
-});
